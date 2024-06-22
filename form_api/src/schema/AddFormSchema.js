aa016f12d371bc9444282c261d722b2d44a0dc2a
Sachin Nagila <sachinnagila2053@gmail.com>
2024-06-22 22:20:28 +1000

    data sent to axios to send make api call

--- form_api/src/schema/AddFormSchema.js
+++ form_api/src/schema/AddFormSchema.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const AddFormSchema = new mongoose.Schema({
   name: { type: String, required: true },
   address: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: String, required: true },
   email: { type: String, required: true },
 });
 export default mongoose.model("form", AddFormSchema);
